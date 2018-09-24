620216bcb7d647b1b9ce610563c0e794400b37b9
asifDurran <42699031+asifDurran@users.noreply.github.com>
2018-09-24 20:33:58 +0200

    Add files via upload

---
+++ website/home.blade.php
@@ -0,0 +1,267 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    
+	<meta http-equiv ="X-UA-comatible" content="IE-edge">
+    
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" href="style.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+   
+   <style>
+   .our_courses{
+    background-color:#924C44;
+    padding:25px;0px;
+    margin-top:25px;
+   }
+     margin-top:25px
+   .team{;
+   }
+   .footer{
+    background-color:black;
+     margin:25px;
+     padding-top:25px ;
+   }
+   .footer p{
+     color:white;
+   }
+   .footer h3{
+     color:white;
+   }
+   .footer ul{
+
+     list-style:none;
+    
+   }
+   .footer ul li a{
+     margin:0;
+     padding:0;
+     font-size:18px;
+     color:white;
+     
+   }
+   
+   </style>
+   
+  </head>
+<body>
+<!--header-->
+    <div class="container-fluid top_bar" >
+    <div class="container">
+      <div class="row">
+       <div class="col-sm-3">
+           <a href="#" class="social-icon"><i class="fa fa-facebook" style="font-size:20px; color:#FFFFFF"></i></a>
+           <a href="#" class="social-icon"><i class="fa fa-twitter" style="font-size:20px; color:#FFFFFF"></i></a>
+           <a href="#" class="social-icon"><i class="fa fa-play-circle" style="font-size:20px; color:#FFFFFF"></i></a>
+           <a href="#" class="social-icon"><i class="fa fa-google-plus" style="font-size:20px; color:#FFFFFF"></i></a>
+       </div>
+       <div class="col-sm-9 text-right contact_info" >
+        <i class="fa fa-envelope"></i> asifdurrani68@gmail.com :
+        <i class="fa fa-phone"></i>+46 750 148 78
+       </div>
+      </div><!--end of first row-->
+    </div><!--end of container fulid-->
+    </div><!--end of container -->
+
+ <!--include logo and navigation -->
+  <div class="container">
+ <div class="row">
+   <div class="col-sm-5">
+       <img src="images/logo.jpg" style="width:200px;heigh:220px;" >
+   </div>
+
+   <div class="col-sm-7  navigation">
+
+   <ul class="nav nav-pills pull-right">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link "  href="#">About US</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link"  href="#">Product</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link "  href="#">Services</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="#">Contact US</a>
+  </li>
+</ul>
+
+   </div>
+ </div><!--end of row-->
+ 
+  </div><!--end of logo container-->
+
+<!--end of logo -->
+ 
+<!--end of herader-->
+
+
+<div class="contaier-fluid">
+<div class="row">
+<img src="images/wallpaper.jpeg" alt="" class="img-fluid">
+
+</div><!--end of row-->
+</div><!--end of container fluid-->
+
+<!--content-->
+<div class="container">
+<div class="row">
+    <div class="col-sm-8" >
+     <h1>It’s Actionable</h1>
+     <p>Have you ever read an article promising you to teach you how to do something and once it was over, 
+        you were still left in the dark? Maybe you understood the importance of using Bing Ads
+        but you didn’t actually know how to even get started creating them.</p>
+        <p>
+        Actionable website content, on the other hand, will be highly valued by readers. It doesn’t just tell them why, 
+        it shows them how. You can use step-by-step tutorials, videos, images, and detailed descriptions to walk users through
+         an idea.
+        </p>
+    </div>
+    <div class="col-sm-4  responsive">
+      <img src="images/languages.jpg" style="height:250px;"class="img-fluid " >
+    </div>
+</div>
+
+</div><!--end of container-->
+
+  <!--courses area-->
+<div class="container-fluid our_courses" >
+<div class="container">
+  <h2 class="text-center">Our Courses</h2>
+  <div class="row">
+   <div class="col-sm-3 col-xs-6">
+       <img src="images/javascript.jpg" alt="" class="img-fluid" >
+   </div>
+   <div class="col-sm-3 col-xs-6">
+      <img src="images/json.png" alt="" class="img-fluid">
+   </div>
+   <div class="col-sm-3 col-xs-6">
+     <img src="images/php.jpg" alt="" class="img-fluid">
+   </div>
+     <div class="col-sm-3 col-xs-6">
+        <img src="images/node.png" alt="" class="img-fluid" >
+    </div> 
+  </div> <!--first row--> 
+<!--second row-->
+    <div class="row second_row" style="margin-top:25px;">
+      <div class="col-sm-3 col-xs-6">
+         <img src="images/java.jpg" alt="" class="img-fluid" >
+      </div>
+      <div class="col-sm-3 col-xs-6">
+         <img src="images/kotline.png" alt="" class="img-fluid">
+      </div>
+       <div class="col-sm-3 col-xs-6">
+         <img src="images/swift.jpg" alt="" class="img-fluid">
+       </div>
+       <div class="col-sm-3 col-xs-6">
+          <img src="images/r.jpg" alt="" class="img-fluid" >
+       </div>
+    </div> 
+        </div><!--end of container --> 
+</div><!--end of container fluid-->
+
+  <!-- end of courses area-->
+
+   <!--out team section -->
+
+
+  <div class="container team">
+  <div class="row">
+    <div class="col-sm-4 ">
+     <div class="thumbnail ">
+       <img src="images/mine.jpg" alt="" class="img-fluid img-rounded">
+
+       <div class="caption">
+         <h3 class="text-center">Asif Durrani</h3>
+         <p>
+         A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea.
+         A paragraph consists of one or more sentences. Though not required by the syntax of any language, 
+         paragraphs are usually an expected part of formal writing, used to organize longer prose.
+         </p>
+        <a href="" class="btn btn-primary"> Contact Durrani</a>
+        <a href="" class="btn btn-primary"> Email Durrani</a>
+      </div>
+     </div><!--end of tumbbline class-->
+    </div>
+    <div class="col-sm-4">
+    <div class="thumbnail">
+       <img src="images/mine.jpg" alt="" class="img-fluid">
+
+       <div class="caption">
+         <h3 class="text-center">Asif Durrani</h3>
+         <p>
+         A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea.
+         A paragraph consists of one or more sentences. Though not required by the syntax of any language, 
+         paragraphs are usually an expected part of formal writing, used to organize longer prose.
+         </p>
+        <a href="" class="btn btn-primary"> Contact Durrani</a>
+        <a href="" class="btn btn-primary"> Email Durrani</a>
+      </div>
+     </div><!--end of tumbbline class-->
+    </div>
+    <div class="col-sm-4">
+    <div class="thumbnail">
+       <img src="images/mine.jpg" alt="" class="img-fluid">
+
+       <div class="caption">
+         <h3 class="text-center">Asif Durrani</h3>
+         <p>
+         A paragraph is a self-contained unit of a discourse in writing dealing with a particular point or idea.
+         A paragraph consists of one or more sentences. Though not required by the syntax of any language, 
+         paragraphs are usually an expected part of formal writing, used to organize longer prose.
+         </p>
+        <a href="" class="btn btn-primary"> Contact Durrani</a>
+        <a href="" class="btn btn-primary"> Email Durrani</a>
+      </div>
+     </div><!--end of tumbbline class-->
+    </div>
+  </div>
+  </div><!--end of container-->
+   <!--end of team section -->
+
+
+   <!--footer -->
+   <div class="container-fluid footer">
+    <div class="container">
+      <div class="row">
+    <div class="col-sm-4">
+     <img src="images/logo.jpg" alt="" class="img-fluid">
+     <p>
+     Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: 
+     </p>
+     <p><a href="#" class="btn btn-danger">Read more</a></p>
+    </div>
+    <div class="col-sm-4">
+   <h3>Quick Links</h3>
+   <ul>
+     <li> <a href="">&#8680;Home</a></li>
+     <li> <a href="">&#8680;About Us</a></li>
+     <li> <a href="">&#8680;Product</a></li>
+     <li> <a href="">&#8680;Services</a></li>
+     <li><a href="">&#8680;Contact us</a></li>
+   
+   </ul>
+   </div>
+   <div class="col-sm-4">
+      <h3>Contact detail</h3>
+  </div>
+
+</div>
+
+    </div><!--end of container -->
+</div><!--end of container fluid -->
+
+
+   <!--end of footer -->
+</body>
+</html>
